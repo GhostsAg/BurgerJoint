6e2e24851bc569b1c13f02312299b00ec08df748
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-13 20:22:35 -0500

    jaws

--- server.js
+++ server.js
@@ -22,6 +22,17 @@ const connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: process.env.myPassword,
+        database: 'burgers_db'
+    });
+};
+
 connection.connect( (err) => {
     if (err) {
         console.error(`Error connecting: ${err.stack}`);
